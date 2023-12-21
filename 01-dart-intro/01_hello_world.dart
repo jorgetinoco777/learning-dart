5a3081e1907a378a45049b1783de619609b94824
jorgetinoco777 <jorgeluistinocoj@gmail.com>
2023-12-21 18:23:16 -0500

    Hello world with dart

---
+++ 01-dart-intro/01_hello_world.dart
@@ -0,0 +1,11 @@
+main() {
+  // var myName = 'Jorge';
+  // late final myName;
+  // String myName = 'Jorge';
+  
+  const myName = 'Jorge';
+  
+  print('Hola $myName');
+  
+  print('Hola ${ myName.toUpperCase() }');
+}
