7d65357b776b7c663fb8a77e7d512c008cc130b6
jorgetinoco777 <jorgeluistinocoj@gmail.com>
2023-12-24 10:53:17 -0500

    List maps and iterables

---
+++ 01-dart-intro/05_list_maps_iterables.dart
@@ -0,0 +1,26 @@
+void main() {
+  
+  // List, Iterables & Set
+  
+  final numbers = [1, 2, 3, 4, 4, 5, 5, 5, 6, 7, 8, 9, 9, 10];
+  
+  print('List original: $numbers');
+  print('Length: ${ numbers.length }');
+  print('Index 0: ${ numbers[0] }');
+  print('First: ${ numbers.first }');
+  print('Last: ${ numbers.last }');
+  
+  final reversedNumbers = numbers.reversed;
+  print('Iterable: $reversedNumbers');
+  print('List: ${ reversedNumbers.toList() }');
+  print('Set: ${ reversedNumbers.toSet() }');
+ 
+  final numbersGreaterThan5 = numbers.where((num) {
+    return num > 5;
+  });
+  
+  print('>5 iterable: $numbersGreaterThan5');
+  print('>5 set: ${ numbersGreaterThan5.toSet() }');
+  
+  
+}
