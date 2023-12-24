0ec79a711f2ccd16f7fa9f4780df2f77f74e7238
jorgetinoco777 <jorgeluistinocoj@gmail.com>
2023-12-24 13:12:19 -0500

    Name constructors

--- 01-dart-intro/07_class_constructor_and_override.dart
+++ 01-dart-intro/07_class_constructor_and_override.dart
@@ -1,16 +1,33 @@
 void main() {
   
+  final Map<String, dynamic> rowJson = {
+    'name': 'Spiderman',
+    'power': 'Trepar paredes',
+    'isAlive': true
+  };
+  
   final Hero wolverine = Hero(name: 'Logan', power: 'Regeneración');
+  final Hero spiderman = Hero.fromJson(rowJson);
   
   print('${wolverine.toString()}');
   print('Name: ${wolverine.name}');
   print('Power: ${wolverine.power}');
+  print('isAlive: ${wolverine.isAlive}');
+  
+  print('');
+  print('${spiderman.toString()}');
+  print('Name: ${spiderman.name}');
+  print('Power: ${spiderman.power}');
+  print('isAlive: ${spiderman.isAlive}');
+  
+  
   
 }
 
 class Hero {
   String name;
   String? power;
+  bool isAlive;
   
   // Option 1
   //Hero(String pName, String pPower): name = pName, power = pPower;
@@ -19,11 +36,18 @@ class Hero {
   //Hero(this.name, this.power);
   
   // Option 3
-  Hero({
+   Hero({
     required this.name,
-      this.power = 'Sin poder'
-  });
+      this.power = 'Sin poder',
+     this.isAlive = false
+   });
+  
+  // Option 4 - name constructor
+  Hero.fromJson( Map<String, dynamic> json )
+    : name = json['name'] ?? 'No name found',
+      power = json['power'] ?? 'No power found',
+      isAlive = json['isAlive'] ?? 'No isAlive found';
   
   @override
-  String toString() => '$name - $power';
+  String toString() => '$name, $power, isAlive: ${ isAlive ? 'YES' : 'NO'}';
 }
